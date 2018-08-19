7deb296893321cc28c33d2dfc018da45d80e83b3
rafaelsartori96 <rafael.sartori96@gmail.com>
2018-08-19 09:34:35 -0300

    Renomeando documentos (para português)

--- django-site/djangosite/paginas_estaticas/views.py
+++ django-site/djangosite/paginas_estaticas/views.py
@@ -5,7 +5,8 @@ from .models import *
 def PaginaEstaticaView(request, pk):
     pagina = PaginaEstatica.objects.get(pk=pk)
     context = {
+        # Para substituirmos as infrmações da página em 'templates/padrao.html'
         'pagina': pagina
     }
 
-    return render(request, 'flatpages/default.html', context=context)
+    return render(request, 'padrao.html', context=context)
