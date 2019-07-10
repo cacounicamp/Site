d78c9f96ae79292a34c9c39c41d80c77c8170cb1
rafaelsartori96 <rafael.sartori96@gmail.com>
2019-07-10 10:25:53 -0300

    Adicionando páginas de erros para todo nginx

--- django-site/djangosite/djangosite/urls.py
+++ django-site/djangosite/djangosite/urls.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.shortcuts import render
 from django.conf.urls.static import static
+
+
+def handle400(request):
+    return render(request, '400.html')
+
+def handle403(request):
+    return render(request, '403.html')
+
+def handle404(request):
+    return render(request, '404.html')
+
+def handle500(request):
+    return render(request, '500.html')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('400/', handle400),
+    path('403/', handle403),
+    path('404/', handle404),
+    path('500/', handle500),
     path('', include('ouvidoria.urls')),
     path('', include('paginas_estaticas.urls')),
     path('', include('noticias.urls')), # Notícias possuirá a raiz do site
