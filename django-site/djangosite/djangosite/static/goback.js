978765347d67f700700fd99b2e14a27fd4a66950
rafaelsartori96 <rafael.sartori96@gmail.com>
2019-08-07 07:21:33 -0300

    Corrigindo goBack() para funcionar com CSP

--- django-site/djangosite/djangosite/static/goback.js
+++ django-site/djangosite/djangosite/static/goback.js
@@ -1,5 +1,10 @@
 
-// Para podermos voltar à página anterior
+// Retorna à página anterior
 function goBack() {
     window.history.back();
 }
+
+// Adiciona função "onClick" de ir à página anterior no elemento de id "goback_button"
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById('goback_button').addEventListener('click', goBack);
+});
